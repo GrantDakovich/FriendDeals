cf42b6384e99ed04d450b272f9f6cfc5d0fcabb9
GrantDakovich <gdakovich@wisc.edu>
2020-02-26 11:12:27 -0600

    challenge change

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+/*
 var FBBotFramework = require('fb-bot-framework');
 // Initialize
 var bot = new FBBotFramework({
@@ -12,6 +17,33 @@ app.use('/webhook', bot.middleware());
 bot.on('message', function(userId, message){
 bot.sendTextMessage(userId, "Echo Message: " + message);
 });
+*/
+
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+  console.log("hit");
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+
+    res.sendStatus(200);
+  }
+});
+
+
+
 app.get("/", function (req, res){
 res.send("hello world");
 });
