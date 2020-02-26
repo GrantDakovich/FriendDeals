694c994adff8cfafa3cfc2a7121748d09e83ddb5
GrantDakovich <gdakovich@wisc.edu>
2020-02-26 10:36:44 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var FBBotFramework = require('fb-bot-framework');
+// Initialize
+var bot = new FBBotFramework({
+page_token: "EAADXTIp7SSkBAFqdZAwak5HDYqMcZCB4KsLlcQMPOcyZB4ChztEjgSkORXF9KZAKXNvBpis6oDsQ5CyRmri64ZAbs5PljjNfJntTTuOiHvqOeaFhyZBrV1QXOk2uDPRdxrr4Y9hBWJzrYuNKcV5bD8ZC86mDxLMla42gaZBSS6ByV0XBX6Er6AYP",
+verify_token: "verify_token"
+});
+// Setup Express middleware for /webhook
+app.use('/webhook', bot.middleware());
+// Setup listener for incoming messages
+bot.on('message', function(userId, message){
+bot.sendTextMessage(userId, "Echo Message: " + message);
+});
+app.get("/", function (req, res){
+res.send("hello world");
+});
+//Make Express listening
+app.listen(3000); 
