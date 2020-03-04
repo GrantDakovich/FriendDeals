fc1515836e63207f890b631ec2dc0ce8914b0797
GrantDakovich <gdakovich@wisc.edu>
2020-03-03 20:38:35 -0600

    webview try #4 (prob wont work)

--- index.js
+++ index.js
@@ -325,10 +325,36 @@ app.post('/webhook', (req, res) => {
 
 });
 
-
-
 app.get("/", function (req, res){
 	res.send("hello world");
 });
+
+app.get('/options', (req, res) => {
+	let referer = req.get('Referer');
+	if (referer) {
+	    if (referer.indexOf('www.messenger.com') >= 0) {
+	        res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
+	    } else if (referer.indexOf('www.facebook.com') >= 0) {
+	        res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
+	    }
+	    res.sendFile('public/options.html', {root: __dirname});
+	}
+});
+
+app.get('/optionspostback', (req, res) => {
+	let body = req.query;
+	let response = {
+	    "text": `Great, I will book you a ${body.bed} bed, with ${body.pillows} pillows and a ${body.view} view.`
+	};
+	res.status(200).send('Please close this window to return to the conversation thread.');
+	callSendAPI(body.psid, response);
+});
+
 //Make Express listening
 app.listen(process.env.PORT || 80);
+
+
+
+
+
+
